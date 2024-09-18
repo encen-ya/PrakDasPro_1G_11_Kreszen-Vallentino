9dfd8f0e45f910ae278ad2e8ca673fd5d6f88013
encen-ya <echenwijono@gmail.com>
2024-09-18 07:32:35 +0700

    P#_Percobaan2_PertanyaanNo3

--- pertemuan3/Kafe11.java
+++ pertemuan3/Kafe11.java
@@ -10,10 +10,10 @@ public class Kafe11 {
         Scanner input = new Scanner(System.in);
 
         boolean keanggotaan;
-        double jmlKopi, jmlTeh, jmlRoti, totalHarga, nominalBayar;
-        double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
-        float diskon = 10 / 100;
-
+        int jmlKopi, jmlTeh, jmlRoti;
+        double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga;
+        float diskon = 10 / 100f;
+        
 
         System.out.print("Masukkan keanggotaan (true/false): ");
         keanggotaan = input.nextBoolean();
@@ -26,11 +26,13 @@ public class Kafe11 {
 
         totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
 
-        nominalBayar = totalHarga - (diskon * totalHarga ); 
-
+        double nominalBayar = totalHarga - (diskon * totalHarga ); 
+        int nominalInt = (int) nominalBayar;
+        
+        
         System.out.println("Keanggotaan pelanggan " + keanggotaan);
         System.out.println("Item pembelian " + jmlKopi + " kopi " + jmlTeh + " teh " + jmlRoti + " roti ");
-        System.out.println("Nominal bayar Rp " + nominalBayar);
+        System.out.println("Nominal bayar Rp " + nominalInt);
     }
 
 }
