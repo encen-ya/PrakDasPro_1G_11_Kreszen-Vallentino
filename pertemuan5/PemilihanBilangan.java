ea200e648f1a75742418e23339aa83849563f680
encen-ya <echenwijono@gmail.com>
2024-09-29 12:47:31 +0700

    P5_Percobaan1_PertnyaanNo1

--- pertemuan5/PemilihanBilangan.java
+++ pertemuan5/PemilihanBilangan.java
@@ -10,10 +10,9 @@ public class PemilihanBilangan {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int angka;
-        String hasil;
         System.out.print("Masukkan sebuah angka: ");
         angka = sc.nextInt();
-        hasil = (angka % 2 == 0) ? "angka " + angka + " termasuk bilangan genap" : "angka " + angka + " termasuk bilangan ganjil"; 
+        String hasil = (angka % 2 == 0) ? "angka " + angka + " termasuk bilangan genap" : "angka " + angka + " termasuk bilangan ganjil"; 
         System.out.println(hasil);
         
     }
