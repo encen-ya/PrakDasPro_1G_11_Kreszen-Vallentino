1f1b172cb7003a509dfe4229e9525cc62d0b014f
encen-ya <echenwijono@gmail.com>
2024-10-02 08:09:01 +0700

    P5_Percobaan2_PertanyaanNo6

---
+++ pertemuan5/PemilihianOperator11.java
@@ -0,0 +1,34 @@
+/**
+ * PemilihianOperator11
+ */
+import java.util.Scanner;
+public class PemilihianOperator11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double angka1, angka2, hasil=0;
+        char operator;
+
+        System.out.print("Masukkan angka1: ");
+        angka1 = sc.nextDouble();
+        System.out.print("masukkan angka2: ");
+        angka2 = sc.nextDouble();
+        System.out.print("operator(+-*/): ");
+        operator = sc.next().charAt(0);
+        
+        if (operator == '+') {
+            hasil = angka1 + angka2;
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+        } else if (operator == '/') {
+            hasil = angka1 / angka2;
+        } else {
+            System.out.println("operator yang dipilih tidak sesuai");
+        }
+        
+        System.out.println(angka1 + " " + operator + " " + angka2 + " = " + hasil);
+    }
+}
