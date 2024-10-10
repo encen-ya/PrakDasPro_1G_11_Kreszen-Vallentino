c5c91bce93e517104933983d8fc7f8a1dc9e48f6
encen-ya <echenwijono@gmail.com>
2024-10-10 08:45:37 +0700

    Pertemuan6_Percobaan1

---
+++ pertemuan6/Pemilihan2Percobaan1_11.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Pemilihan2Percobaan1_11
+ */
+public class Pemilihan2Percobaan1_11 {
+
+    public static void main(String[] args) {
+        Scanner i = new Scanner(System.in);
+
+        int tahun;
+        System.out.print("masukkan tahun : ");
+        tahun = i.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100 != 0)) {
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan Tahun Kabisat");
+        } 
+
+    }
+}
