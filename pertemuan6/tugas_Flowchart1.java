696d343beeda6b1b6c397b3cf0dec348ff183ae1
encen-ya <echenwijono@gmail.com>
2024-10-10 15:02:04 +0700

    TugasP6_flowchart1

---
+++ pertemuan6/tugas_Flowchart1.java
@@ -0,0 +1,27 @@
+public class tugas_Flowchart1 {
+    public static void main(String[] args) {
+
+                int bil1 = 28;
+                int bil2 = 54;
+                int bil3 = 15;
+        
+                int terbesar;
+        
+                if (bil1 > bil2) {
+                    if (bil1 > bil3) {
+                        terbesar = bil1;
+                    } else {
+                        terbesar = bil3;
+                    }
+                } else {
+                    if (bil2 > bil3) {
+                        terbesar = bil2;
+                    } else {
+                        terbesar = bil3;
+                    }
+                }
+        
+                System.out.println("Angka terbesar adalah: " + terbesar);
+            }
+        }
+        
