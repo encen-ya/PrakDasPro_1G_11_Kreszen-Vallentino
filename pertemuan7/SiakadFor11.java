406546288657a067c1cadb45449b2f22792fe1ab
encen-ya <echenwijono@gmail.com>
2024-10-11 14:32:06 +0700

    Pertemuan7_Percobaan1_PertanyaanNo4

--- pertemuan7/SiakadFor11.java
+++ pertemuan7/SiakadFor11.java
@@ -3,7 +3,10 @@ public class SiakadFor11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 100, terendah = 0;
+        int lulus = 0, tidakLulus = 0;
+        double nilai, tertinggi = 0, terendah = 100, batasKelulusan = 60;
+
+
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
@@ -12,9 +15,15 @@ public class SiakadFor11 {
             }
             if (nilai < terendah) {
                 terendah = nilai;
+            } if (nilai >= batasKelulusan) {
+                lulus++;
+            } if (nilai < batasKelulusan) {
+                tidakLulus++;
             }
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa yang tidak lulus: " + tidakLulus);
     }
 }
