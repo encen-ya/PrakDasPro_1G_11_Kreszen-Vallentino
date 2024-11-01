970bfa511cb9676625f1824ea5f6390054259f92
encen-ya <echenwijono@gmail.com>
2024-11-01 14:33:27 +0700

    P10_Percobaan1_PertanyaanNo2

--- pertemuan10/ArrayBilangan11.java
+++ pertemuan10/ArrayBilangan11.java
@@ -1,10 +1,6 @@
 public class ArrayBilangan11 {
     public static void main(String[] args) {
-        int  bil [] = new int[4];
-        bil[0] = 5.0;
-        bil[1] = 12867;
-        bil[2] = 7.5;
-        bil[3] = 2_000_000;
+        int  bil [] = {5,13,-7,17};
         System.out.println(bil[0]);
         System.out.println(bil[1]);
         System.out.println(bil[2]);
