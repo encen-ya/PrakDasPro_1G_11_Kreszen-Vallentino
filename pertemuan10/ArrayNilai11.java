50ae62ebc73495352ce31ea7829cdcb48c49236b
encen-ya <echenwijono@gmail.com>
2024-11-05 19:16:02 +0700

    P10_Percobaan2_PertanyaanNo1

--- pertemuan10/ArrayNilai11.java
+++ pertemuan10/ArrayNilai11.java
@@ -6,13 +6,13 @@ public class ArrayNilai11 {
         Scanner sc = new Scanner(System.in);
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai Akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai Akhir ke-" + i + " adalah " + nilaiAkhir[i]);             
-        }
+                System.out.println("Nilai Akhir ke-" + i + " adalah " + nilaiAkhir[i]);             
+            }
         
     }
 }
