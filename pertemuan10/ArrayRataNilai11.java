33d0f3a477cf3cc4c21b734a5d1863e6ee8139ca
encen-ya <echenwijono@gmail.com>
2024-11-05 20:16:21 +0700

    P10_Percobaan3_PertanyaanNo1

--- pertemuan10/ArrayRataNilai11.java
+++ pertemuan10/ArrayRataNilai11.java
@@ -6,6 +6,7 @@ public class ArrayRataNilai11 {
         int[] nilaiMhs = new int [10];
         double total = 0;
         double rata2;
+        int lulus = 0;
 
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai Mahasiswa ke-" + (i + 1) + " : ");
@@ -13,8 +14,12 @@ public class ArrayRataNilai11 {
         }
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                lulus++;
+            }
         }
         rata2 = total / nilaiMhs.length;
         System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Jumlah mahasiswa yang lulus = " + lulus);
     }
 }
