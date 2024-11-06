962c98185392855eecc40dc3a0df60c72e86ed45
encen-ya <echenwijono@gmail.com>
2024-11-06 19:59:13 +0700

    P10_Percobaan4_PertanyaanNo3

--- pertemuan10/SearchNilai11.java
+++ pertemuan10/SearchNilai11.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class SearchNilai11 {
     public static void main(String[] args) {
-        int[] arrNilai ={80, 85 ,78 ,96 ,90 ,82 ,86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int bykNilai = sc.nextInt();
+        int[] arrNilai = new int[bykNilai];
+        int key = 0;
+        int hasil = -1;
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        key = sc.nextInt();
 
         for (int i = 0; i < arrNilai.length; i++) { 
             if (key == arrNilai[i]) {
@@ -10,8 +23,13 @@ public class SearchNilai11 {
                 break;
             }
         }
+
         System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil );
+        if (hasil == -1) {
+            System.out.println("Nilai yang dicari tidak ditemukan.");
+        } else {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        }
         System.out.println();
     }
 }
