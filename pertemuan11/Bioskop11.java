00d74c707661f62ec39674d4b12f75b751590104
encen-ya <echenwijono@gmail.com>
2024-11-08 07:50:29 +0700

    P11_Percobaan1_PertanyaanNo3

--- pertemuan11/Bioskop11.java
+++ pertemuan11/Bioskop11.java
@@ -9,6 +9,8 @@ public class Bioskop11 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
+        penonton[3][0] = "Hana";
+
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
         System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
