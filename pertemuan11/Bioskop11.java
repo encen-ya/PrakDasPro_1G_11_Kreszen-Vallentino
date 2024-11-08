57c77acee96947791edf06735837a9dbf73571a5
encen-ya <echenwijono@gmail.com>
2024-11-08 08:44:21 +0700

    P11_Percobaan1_PertanyaanNo9

--- pertemuan11/Bioskop11.java
+++ pertemuan11/Bioskop11.java
@@ -9,7 +9,7 @@ public class Bioskop11 {
         penonton[2][0] = "Eka";
         penonton[2][1] = "Farhan";
         penonton[3][0] = "Gisel";
-        penonton[3][0] = "Hana";
+        penonton[3][1] = "Hana";
 
 
         System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
@@ -18,9 +18,10 @@ public class Bioskop11 {
         System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
 
         System.out.println(penonton.length);
-        for (String[] barisPenonton : penonton) {
-            System.out.println("Panjang baris: " + barisPenonton.length);
-        }
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke- " + (i + 1) + ": " + String.join(", ", penonton[i]));
+        } 
+        
         System.out.println("Penonton pada baris ke-3: ");
         for (String i : penonton[2]) {
             System.out.println(i);
