0360fdbd92d126f35ec3e6b37e61a7cf0c28b99e
encen-ya <echenwijono@gmail.com>
2024-11-13 20:19:07 +0700

    P11_Percobaan2_PeertanyaanNo2

--- pertemuan11/BioskopWithScanner11.java
+++ pertemuan11/BioskopWithScanner11.java
@@ -5,25 +5,48 @@ public class BioskopWithScanner11 {
         Scanner sc = new Scanner(System.in);
         int baris, kolom;
         String nama, next;
-
         String[][] penonton = new String[4][2];
+        int menu;
 
         while (true) {
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
+            System.out.print("Menu:");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            menu = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            switch (menu) {
+                case 1:
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                    System.out.print("Masukkan baris(1-4): ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom(1-2): ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
+                    penonton[baris-1][kolom-1] = nama;
+                    System.out.println("Data penonton berhasil ditambahkan.");
+                    break;
 
-            if (next.equalsIgnoreCase("n")) {
-                break;
+                case 2:
+                    System.out.println("Daftar penonton:");
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[i].length; j++) {
+                            System.out.println("Baris " + (i + 1) + " Kolom " + (j + 1) + ": " + penonton[i][j]);
+                        }
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Keluar dari program.");
+                    return;
+
+                default:
+                    System.out.println("Silakan pilih menu yang tersedia.");
+                    break;
             }
         }
     }
