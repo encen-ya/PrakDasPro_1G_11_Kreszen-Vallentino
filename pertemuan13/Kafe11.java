22574fdfd278e7f30134e136017018590f88fe27
encen-ya <echenwijono@gmail.com>
2024-11-29 14:40:49 +0700

    P13_Percobaan2_PertanyaanNo5

--- pertemuan13/Kafe11.java
+++ pertemuan13/Kafe11.java
@@ -1,11 +1,16 @@
 public class Kafe11 {
-    public static void menu(String namaPelanggan, Boolean isMember) {
+    public static void menu(String namaPelanggan, Boolean isMember, String KodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian");
             
         }
+        if (KodePromo.equalsIgnoreCase("DISKON50")) {
+            System.out.println("Diskon 50%");
+       } else if (KodePromo.equalsIgnoreCase("DISKON30")) {
+            System.out.println("Diskon 30%");
+       }
             System.out.println("===== MENU RESTO KAFE =====");
             System.out.println("1. Kopi Hitam - Rp 15,000");
             System.out.println("2. Cappuccino - Rp 20,000");
@@ -17,6 +22,6 @@ public class Kafe11 {
             System.out.println("Silahakan Pilih Menu Yang Anda inginkan");
     }
     public static void main(String[] args) {
-        menu("Andi", true);
+        menu("Andi", true, "Diskon50");
     }
 }
