a88f94d38bf00db6e5d11c3fc1fcf945f150e57b
encen-ya <echenwijono@gmail.com>
2024-11-29 13:47:54 +0700

    P13_Percobaan1_PertanyaanNo2

--- pertemuan13/Kafe11.java
+++ pertemuan13/Kafe11.java
@@ -1,5 +1,5 @@
 public class Kafe11 {
-    public static void menu() {
+    public static void main(String[] args) {
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -9,8 +9,5 @@ public class Kafe11 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silahakan Pilih Menu Yang Anda inginkan");
-    }
-    public static void main(String[] args) {
-        menu();
     }
 }
