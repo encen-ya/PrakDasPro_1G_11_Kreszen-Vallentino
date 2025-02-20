2db8a9cc4e4086123f65ce38a173419923f1a83f
encen-ya <echenwijono@gmail.com>
2025-02-20 08:42:30 +0700

    P2_Percobaan1

---
+++ jobsheet2/mahasiswa11.java
@@ -0,0 +1,35 @@
+public class mahasiswa11 {
+        String nama;
+        String nim;
+        String kelas;
+        double ipk;
+
+        void tampilkanInformasi() {
+            System.out.println("Nama: " + nama);
+            System.out.println("nim: " + nim);
+            System.out.println("nim: " + ipk);
+            System.out.println("nim: " + kelas);
+        }
+
+        void ubahKelas(String kelasBaru) {
+            kelas = kelasBaru;
+        }
+
+        void updateIpk(double ipkBaru) {
+            ipk = ipkBaru;
+        }
+
+        String nilaiKinerja() {
+            if (ipk >= 3.5) {
+                return "Kinerja sangat baik";
+            } else if (ipk >= 3.0)   {
+                return "Kinerja baik";
+            } else if (ipk >= 2.0) {
+                return "Kinerja cukup";
+            } else if (ipk < 0.0 && ipk > 4.0) {
+                return "IPK tidak valid. Harus antara 0.0 dan 4.0 ";
+            } else {
+                return "Kinerja kurang";
+            }
+        }
+}
