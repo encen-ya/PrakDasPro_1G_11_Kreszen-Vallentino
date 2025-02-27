f5e8ba8346c2bfebb54e76ccc7b191007a518bbf
encen-ya <echenwijono@gmail.com>
2025-02-27 11:09:36 +0700

    P3_Percobaan3_Pertanyaan2

--- Praktikum03/Mahasiswa11.java
+++ Praktikum03/Mahasiswa11.java
@@ -12,3 +12,5 @@ public class Mahasiswa11 {
         System.out.println("--------------------------------------");
     }
 }
+
+
--- Praktikum03/MataKuliah11.java
+++ Praktikum03/MataKuliah11.java
@@ -1,17 +1,33 @@
+import java.util.Scanner;
+
 public class MataKuliah11 {
+    Scanner sc = new Scanner(System.in);
     public String kode;
     public String nama;
     public int sks;
     public int jumlahJam;
+
     public MataKuliah11(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+
     }
     public MataKuliah11() {
-        
+
     }
+    
+    void tambahData(Scanner sc) {
+        System.out.print("Kode        : ");
+        this.kode = sc.nextLine();
+        System.out.print("Nama        : ");
+        this.nama = sc.nextLine();
+        System.out.print("sks         : ");
+        this.sks = Integer.parseInt(sc.nextLine());
+        System.out.print("Jumlah Jam  : ");
+        this.jumlahJam = Integer.parseInt(sc.nextLine());
+        System.out.println("-----------------------------");
 }
 
-
+}
