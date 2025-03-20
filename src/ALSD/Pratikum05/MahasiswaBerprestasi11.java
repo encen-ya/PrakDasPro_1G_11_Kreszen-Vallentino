2ba46f2f1e3aea3ecc10316381d3d6997cc59b17
encen-ya <echenwijono@gmail.com>
2025-03-20 12:07:09 +0700

    P6_Percobaan2_Pertanyaan_Insertion

--- src/ALSD/Pratikum05/MahasiswaBerprestasi11.java
+++ src/ALSD/Pratikum05/MahasiswaBerprestasi11.java
@@ -63,7 +63,7 @@ void insertionSort() {
     for (int i = 1; i < listMhs.length; i++) {
         Mahasiswa11 temp = listMhs[i];
         int j = i;
-        while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+        while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
             listMhs[j] = listMhs[j - 1];
             j--;
         }
