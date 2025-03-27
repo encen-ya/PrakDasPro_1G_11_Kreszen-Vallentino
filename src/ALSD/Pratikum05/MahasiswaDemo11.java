6443c25ce093aa29bed2d20a6766a0c39872951d
encen-ya <echenwijono@gmail.com>
2025-03-27 11:17:44 +0700

    P7_Percobaan2

--- src/ALSD/Pratikum05/MahasiswaDemo11.java
+++ src/ALSD/Pratikum05/MahasiswaDemo11.java
@@ -26,17 +26,36 @@ public class MahasiswaDemo11 {
 
         list.tampil();
 
-        System.out.println("------------------------------------------------");
-        System.out.println("Pencarian data");
-        System.out.println("------------------------------------------------");
-        System.out.print("Masukkan IPK mahasiswa yang dicari: ");
-        double cari = sc.nextDouble();
+        // System.out.println("------------------------------------------------");
+        // System.out.println("Pencarian data");
+        // System.out.println("------------------------------------------------");
+        // System.out.print("Masukkan IPK mahasiswa yang dicari: ");
+        // double cari = sc.nextDouble();
 
-        System.out.println("Menggunakan sequential searching");
-        double posisi = list.sequentialSearching(cari);
-        int pss = (int) posisi;
-        list.tampilPosisi(cari, pss);
-        list.tampilDataSearch(cari, pss);
+        // System.out.println("Menggunakan sequential searching");
+        // double posisi = list.sequentialSearching(cari);
+        // int pss = (int) posisi;
+        // list.tampilPosisi(cari, pss);
+        // list.tampilDataSearch(cari, pss);
+
+
+        
+            System.out.println("--------------------------------------------------");
+            System.out.println("Pencarian data");
+            System.out.println("--------------------------------------------------");
+            System.out.print("Masukkan IPK mahasiswa yang dicari: ");
+            System.out.print("IPK : ");
+            double cari = sc.nextDouble();
+
+            System.out.println("--------------------------------------------------");
+            System.out.println("Menggunakan binary search");
+            System.out.println("--------------------------------------------------");
+
+            double posisi2 = list.findBinarySearch(cari, 0, jumMhs - 1);
+            int pss2 = (int) posisi2;
+            list.tampilPosisi(cari, pss2);
+            list.tampilDataSearch(cari, pss2);
+
         
     }
     
