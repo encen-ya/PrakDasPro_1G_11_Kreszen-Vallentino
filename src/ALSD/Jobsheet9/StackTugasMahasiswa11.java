9ea7e197ae33bfa4258c985918a166162e5c9a33
encen-ya <echenwijono@gmail.com>
2025-05-01 09:21:23 +0700

    P10_Percobaan1_Pertanyaan1

--- src/ALSD/Jobsheet9/StackTugasMahasiswa11.java
+++ src/ALSD/Jobsheet9/StackTugasMahasiswa11.java
@@ -56,9 +56,10 @@ public Mahasiswa11 peek() {
 }
 
 public void print() {
-    for (int i = 0; i <= top; i++) {
-        System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
-    }
+    for (int i = top; i >= 0; i--) {
+    System.out.println(stack[i].nama + "\t" + stack[i].nim + "\t" + stack[i].kelas);
+}
+
     System.out.println("");
 }
 
