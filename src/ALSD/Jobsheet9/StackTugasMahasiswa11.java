90d75ce2422d4502df1bf7a09a9875951a1ff1e2
encen-ya <echenwijono@gmail.com>
2025-05-01 10:50:30 +0700

    P10_Percobaan2

--- src/ALSD/Jobsheet9/StackTugasMahasiswa11.java
+++ src/ALSD/Jobsheet9/StackTugasMahasiswa11.java
@@ -69,10 +69,23 @@ public Mahasiswa11 bawah() {
         return null;
     }
 }
-public int jumlahTugas() {
-    return top + 1;
-}
-
+    public int jumlahTugas() {
+        return top + 1;
+    }
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi11 stack = new StackKonversi11();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+    
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 
 
 }
