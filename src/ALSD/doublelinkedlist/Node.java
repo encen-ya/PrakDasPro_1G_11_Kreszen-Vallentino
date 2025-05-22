fee85fa632b4b8d6603719597ab2f56d5e156071
encen-ya <echenwijono@gmail.com>
2025-05-22 08:33:34 +0700

    P13_Percobaan1

---
+++ src/ALSD/doublelinkedlist/Node.java
@@ -0,0 +1,15 @@
+package ALSD.doublelinkedlist;
+
+public class Node {
+    int data;
+    Node prev, next;
+
+    Node(Node prev, int data, Node next) {
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+    Node() {
+
+    }
+}
