54843379153777058d072eb09aba0d456ca60627
encen-ya <echenwijono@gmail.com>
2025-06-05 04:43:14 +0700

    P15_Percobaan1

---
+++ src/ALSD/Pertemuan14/Node11.java
@@ -0,0 +1,14 @@
+package ALSD.Pertemuan14;
+
+public class Node11 {
+    Mahasiswa11 mahasiswa;
+    Node11 left, right;
+
+    public Node11() {
+    }
+
+    public Node11(Mahasiswa11 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
